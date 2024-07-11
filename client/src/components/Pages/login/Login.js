6ec19a61eea6b1d11f1170835dcf0acd950684fd
Vegetablegito247 <Yoshimitsu247Kakarot@gmail.com>
2024-07-10 18:48:30 -0600

    handled login navigation

--- client/src/components/Pages/login/Login.js
+++ client/src/components/Pages/login/Login.js
@@ -13,15 +13,48 @@ import { toast } from 'react-toastify'
 import { Spinner } from 'reactstrap'
 
 function Login() {
-  
-
-
   const [show, setShow] = useState(false)
   const [success, setSuccess] = useState(false)
   const [page, setpage] = useState(1)
   const [email, setEmail] = useState("")
   const [showforgetpassword, setShowForgetPassword] = useState(false)
   const [showforgetpasswordStatus, setShowForgetPasswordStatus] = useState(false)
+
+  const [logData, setLogData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [error, setError] = useState({});
+
+  const validateUser = () => {
+    const newErrors = {};
+
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(logData.email.trim())) {
+      newErrors.email = 'Invalid email address';
+    }
+    else if (!logData.email.trim()) {
+      newErrors.email = 'Email account is required';
+    }
+    if (!logData.password.trim()) {
+      newErrors.password = 'Password is required';
+    }
+    else if (logData.password.length < 3) {
+      newErrors.password = 'Password must be at least 3 characters';
+    }
+
+    setError(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLogData({
+        ...logData,
+        [name]: value
+    });
+};
 
   const toggle = () => {
     setShowForgetPassword((prev) => !prev)
@@ -30,28 +63,27 @@ function Login() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-
     setSuccess(true)
-    const form = new FormData(e.currentTarget)
-
-    await axios.post(`${process.env.REACT_APP_API_URL}/login`, form)
+    console.log(logData)
+    
+    if(validateUser()){
+      axios.post(`https://lasopbackend.net/login`, logData)
       .then((res) => {
         if (res?.data?.message === "login successful") {
-          setSuccess(false)
+          setSuccess(true)
           dispatch(loginCtrl(res?.data?.token))
           dispatch(infoCtrl(res?.data?.data))
-          navigate("/")
+          navigate("/signup")
           toast.success("login successful")
         }
       })
-      .catch((err) =>{
-         setSuccess(false)
-         toast.error(err?.response?.data)
+      .catch((err) => {
+        setSuccess(false)
+        toast.error(err?.response?.data)
       })
-
-
+    }
   }
 
   const handleForgetPassword = async (e) => {
@@ -59,9 +91,7 @@ function Login() {
 
     setShowForgetPasswordStatus(true)
 
-    const form = new FormData(e.currentTarget)
-
-    await axios.post(`${process.env.REACT_APP_API_URL}/forgetpassword`, form)
+    await axios.post(`https://lasopbackend.net/forgetpassword`, logData)
       .then((res) => {
         setEmail(res?.data?.email)
         setShowForgetPasswordStatus(false)
@@ -79,18 +109,18 @@ function Login() {
     setShowForgetPasswordStatus(true)
 
     const form = new FormData(e.currentTarget)
-    if( form.get("password") === "" ){
+    if (form.get("password") === "") {
       toast.warn("please enter  password")
       setShowForgetPasswordStatus(false)
     }
-    else if(form.get('password') !== form.get("retype")){
+    else if (form.get('password') !== form.get("retype")) {
       toast.warn("password do not match")
       setShowForgetPasswordStatus(false)
-    }else{
+    } else {
 
       form.append("email", email)
 
-      await axios.post(`${process.env.REACT_APP_API_URL}/changepassword`, form)
+      await axios.post(`https://lasopbackend.net/changepassword`, form)
         .then((res) => {
           setShowForgetPasswordStatus(false)
           toast.success("password changed successfully")
@@ -107,35 +137,35 @@ function Login() {
 
   return (
     <>
-      <div  className="login d-flex flex-column justify-content-center align-items-center">
+      <div className="login d-flex flex-column justify-content-center align-items-center">
         <div className="container-fluid">
 
           <div className="row align-items-lg-center  ">
             <div className="p-md-5 p-2  col-12 col-md-6 position-relative">
-             
-            
+
+
 
               <p className='heading m-4 m-md-0'>WELCOME BACK</p>
 
               <div className="loginform  p-4">
                 <p>Input Your Details</p>
 
-                <form onSubmit={(e) => handleSubmit(e)}>
+                <form onSubmit={handleSubmit}>
                   <div className="my-1">
                     <div className="form-label d-block">Email</div>
-                    <input type="email" className="" name="email" />
+                    <input type="email" className="" name="email" onChange={handleChange} />
                   </div>
 
                   <div className="pwd my-1">
                     <div className="form-label d-block">Password</div>
                     <div className="d-flex align-items-center position-relative">
-                      <input type={`${show === false ? "password" : "text"}`} className="position-absolute" name="password" />
+                      <input type={`${show === false ? "password" : "text"}`} className="position-absolute" name="password" value={logData.password} onChange={handleChange} />
                       <AiOutlineEyeInvisible onClick={(e) => setShow(!show)} className='d-block ms-auto position-relative left-100' size={28} />
                     </div>
                   </div>
 
                   <button className='my-3 btn btn-primary w-100'>
-                    {success ? <Spinner/> : "Login"}
+                    Login
                   </button>
 
                 </form>
@@ -151,7 +181,7 @@ function Login() {
 
                     <div className="container">
                       {page === 1 ?
-                        <form onSubmit={(e) => handleForgetPassword(e)}>
+                        <form onSubmit={handleForgetPassword}>
 
                           <input name='email' type="email" placeholder='enter your email' className="form-control" />
 
@@ -170,16 +200,16 @@ function Login() {
                         </form>
                         :
 
-                        <form  onSubmit={(e) => handlePasswordChange(e)}>
-                         <div>
-                         <input name='password' type="password" className="form-control" />
-                         <label htmlFor="" className="form-label">new password</label>
-                         </div>
-                         
-                         <div className='my-2'>
-                         <input name='retype' type="password" className="form-control" />
-                         <label htmlFor="" className="form-label">Retype Password</label>
-                         </div>
+                        <form onSubmit={handlePasswordChange}>
+                          <div>
+                            <input name='password' type="password" className="form-control" />
+                            <label htmlFor="" className="form-label">new password</label>
+                          </div>
+
+                          <div className='my-2'>
+                            <input name='retype' type="password" className="form-control" />
+                            <label htmlFor="" className="form-label">Retype Password</label>
+                          </div>
 
                           <button className="btn btn-dark my-3">
                             {showforgetpasswordStatus === true ?
