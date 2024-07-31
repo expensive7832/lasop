e1a5245c97b42dd3b6b53ac710af543fd6f44d48
Vegetablegito247 <Yoshimitsu247Kakarot@gmail.com>
2024-07-30 21:14:26 -0600

    Updated stack price

--- client/src/components/Pages/course/Courses.js
+++ client/src/components/Pages/course/Courses.js
@@ -10,57 +10,57 @@ import { TbMathGreater } from 'react-icons/tb'
 
 function Courses() {
 
-    const cardsData = [
-        {
-          title: "fullstack development",
-          info: "Learn to create professional, responsive websites using HTML, CSS, Bootstrap, JavaScript, JQuery, React, Python, Django & SQL.",
-          index: 2,
-          img: "full.png",
-    
-        },
-        {
-          title: "mobile app development",
-          info: "Learn to create mobile UI designs with native frameworks or cross-platform frameworks, React Native, Flutter",
-          index: 4,
-          img: "mob.png"
-        },
-        {
-          title: "frontend development",
-          info: "Learn to create professional, responsive websites using HTML, CSS, Bootstrap, JavaScript, JQuery, React, & SQL.",
-          index: 1,
-          img: "full.png"
-        },
-        {
-          title: "backend development",
-          info: "Learn Python, and its framework Django. Or  Node and Express.NET if your interest is to become a Nodejs developer.",
-          index: 3,
-          img: "backend.png"
-        },
-        {
-          title: "UI/UX design",
-          info: "Learn design thinking, wireframes, interactive prototyping. Earn a UX design certification to accelerate your career with cutting-edge skills.",
-          index: 0,
-          img: "ui.png"
-        },
-        {
-          title: "Data science and AI",
-          info: "Dive into prescriptive and predictive analytics, machine learning, artificial intelligence, statistical analysis, and programming languages.",
-          index: 5,
-          img: "ds.png"
-        },
-      ]
+  const cardsData = [
+    {
+      title: "fullstack development",
+      info: "Learn to create professional, responsive websites using HTML, CSS, Bootstrap, JavaScript, JQuery, React, Python, Django & SQL.",
+      index: 2,
+      img: "full.png",
+
+    },
+    {
+      title: "mobile app development",
+      info: "Learn to create mobile UI designs with native frameworks or cross-platform frameworks, React Native, Flutter",
+      index: 4,
+      img: "mob.png"
+    },
+    {
+      title: "frontend development",
+      info: "Learn to create professional, responsive websites using HTML, CSS, Bootstrap, JavaScript, JQuery, React, & SQL.",
+      index: 1,
+      img: "full.png"
+    },
+    {
+      title: "backend development",
+      info: "Learn Python, and its framework Django. Or  Node and Express.NET if your interest is to become a Nodejs developer.",
+      index: 3,
+      img: "backend.png"
+    },
+    {
+      title: "UI/UX design",
+      info: "Learn design thinking, wireframes, interactive prototyping. Earn a UX design certification to accelerate your career with cutting-edge skills.",
+      index: 0,
+      img: "ui.png"
+    },
+    {
+      title: "Data science and AI",
+      info: "Dive into prescriptive and predictive analytics, machine learning, artificial intelligence, statistical analysis, and programming languages.",
+      index: 5,
+      img: "ds.png"
+    },
+  ]
 
   return (
-   <div className="courses">
-   <Navbar/>
+    <div className="courses">
+      <Navbar />
 
-   <div className="content container p-5">
+      <div className="content container p-5">
 
-    <h3 data-aos="fade-right" className="heading text-center">Courses Offered</h3>
-    <img  src={doubleline} className="d-block doubleline img-fluid" alt="outline" />
-   
+        <h3 data-aos="fade-right" className="heading text-center">Courses Offered</h3>
+        <img src={doubleline} className="d-block doubleline img-fluid" alt="outline" />
 
-    <div data-aos="zoom-in" className="cards container">
+
+        <div data-aos="zoom-in" className="cards container">
           <div className="row gap-md-0 gap-3">
             {cardsData?.map((data, i) => (
               <div key={i} className="col-md-4 my-2">
@@ -68,14 +68,14 @@ function Courses() {
                   <img src={`./../../../images/${data?.img}`} alt={data?.title} className="card-img-top img-fluid" />
                   <h5 className='my-3 text-capitalize '>{data?.title}</h5>
                   <p>{data?.info}</p>
-                 <div className="align-items-center mt-3 d-flex justify-content-between">
-                 <div className="border w-50 rounded px-1 border-primary d-flex p-1 align-items-center">
-                    <Link to={`/course/${data?.index}`} className='fw-bold'>learn more</Link>
-                    <TbMathGreater color='#000066' />
-                  </div>
+                  <div className="align-items-center mt-3 d-flex justify-content-between">
+                    <div className="border w-50 rounded px-1 border-primary d-flex p-1 align-items-center">
+                      <Link to={`/course/${data?.index}`} className='fw-bold'>learn more</Link>
+                      <TbMathGreater color='#000066' />
+                    </div>
 
-                  <Link to="/signup" className="btn btn-primary">Apply</Link>
-                 </div>
+                    <Link to="/signup" className="btn btn-primary">Apply</Link>
+                  </div>
                 </div>
               </div>
             ))}
@@ -85,10 +85,10 @@ function Courses() {
 
 
 
-   </div>
+      </div>
 
-   <Footer/>
-   </div>
+      <Footer />
+    </div>
   )
 }
 
